2af53f4ed22cb02d64ebe8ffd2f489fe074ad4fb
domiguo818 <1193224158@qq.com>
2022-07-03 20:52:10 +0800

    完成了todos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,83 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <TodoHeader @add='addFn' @setchecked="setcheckedFn"></TodoHeader>
+    <TodoMain :list='checked' @del1='delFn'></TodoMain>
+    <TodoFooter :count='count' @filterdata='filterdataFn' @clear="clearFun"></TodoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import TodoFooter from './components/TodoFooter.vue'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoMain from './components/TodoMain.vue'
+export default { 
+  data(){
+    return{
+      list:JSON.parse(localStorage.getItem('list')),
+      //  [
+      //   { id: 100, name: "吃饭", isDone: true },
+      //   { id: 201, name: "睡觉", isDone: false },
+      //   { id: 103, name: "打豆豆", isDone: true },
+      // ],
+      getSel :'all'
+    }
+  },
+  components:{
+    TodoFooter,
+    TodoHeader,
+    TodoMain
+  },
+  methods: {
+    
+    addFn(val){
+      const id =this.list[this.list.length-1]?this.list[this.list.length-1].id+1:100
+      this.list.push({
+        name:val,
+        isDone:false,
+        id,
+      })
+    },
+    delFn(id){
+      const index = this.list.findIndex(ele=>ele.id==id)
+      this.list.splice(index,1)
+    },
+    filterdataFn(val){
+      this.getSel=val
+      console.log(this.getSel);
+    },
+     clearFun(){ // 清除已完成
+          this.list = this.list.filter(obj => obj.isDone == false)
+        },
+        setcheckedFn(val){
+          this.list.forEach(ele=>ele.isDone=val)
+        }
+  },
+  computed:{
+    count(){
+      return this.list.filter(ele=> !ele.isDone).length
+    },
+    checked(){
+      if(this.getSel=="no"){
+      return this.list.filter(ele=> !ele.isDone)}
+      else if(this.getSel=="yes"){
+        return this.list.filter(ele=>ele.isDone)
+      }else{
+        return this.list
+      }
+    }
+   
+  },
+  watch:{
+    list:{
+      deep:true,
+      handler(val){
+        localStorage.setItem('list',JSON.stringify(val || []))
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='less' scoped>
+
 </style>
---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="footer">
+    <span class="todo-count">剩余{{count}}<strong>数量值</strong></span>
+    <ul class="filters">
+      <li>
+        <a :class="{selected: isSel =='all'}" href="javascript:;" 
+       @click="change('all')" >全部</a>
+      </li>
+      <li>
+        <a  :class="{selected: isSel =='no'}" href="javascript:;"
+        @click="change('no')">未完成</a>
+      </li>
+      <li>
+        <a :class="{selected: isSel =='yes'}" href="javascript:;" 
+        @click="change('yes')">已完成</a>
+      </li>
+    </ul>
+    <button class="clear-completed" @click="clearFn">清除已完成</button>
+  </footer>
+</template>
+
+<script>
+export default {
+ props:['count'],
+ data(){
+    return{
+        isSel:'all'
+    }
+ },
+ methods:{
+    change(val){
+        this.isSel=val;
+        this.$emit('filterdata',val)
+    },
+    clearFn(){ // 清空已完成任务
+          // 7.1 触发App.vue里事件对应clearFun方法
+          this.$emit('clear')
+        }
+ },
+
+}
+</script>
