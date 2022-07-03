c8cbba8f0a5d051f5b9ee5a7620167b4807974c9
kelly227800 <2529303733@qq.com>
2022-07-03 21:24:22 +0800

    搭建了基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="todoapp">
+    <TodoHeader></TodoHeader>
+    <TodoMain></TodoMain>
+    <TodoFooter></TodoFooter>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoHeader from './components/TodoHeader'
+import TodoMain from './components/TodoMain'
+import TodoFooter from './components/TodoFooter'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    TodoHeader,
+    TodoMain,
+    TodoFooter,
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <h1>todos</h1>
+    <input id="toggle-all" class="toggle-all" type="checkbox" >
+    <label for="toggle-all"></label> 
+    <!-- label 可以关联一个表单标签 -->
+    <input
+      class="new-todo"
+      placeholder="输入任务名称-回车确认"
+      autofocus
+    />
+  </header>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
