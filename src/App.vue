64d2005208dc0d17003d5c8dee28f60530effe67
lxh15 <1392649188@qq.com>
2022-07-04 23:36:45 +0800

    购物车案例完成了删除功能

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <table border="1" width="700" style="border-collapse: collapse">
+      <caption>
+        购物车
+      </caption>
+      <thead>
+        <tr>
+          <th><input type="checkbox" /> <span>全选</span></th>
+          <th>名称</th>
+          <th>价格</th>
+          <th>数量</th>
+          <th>总价</th>
+          <th>操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <MyCar v-for="(item, index) in goodList" :key="index" :item="item">
+        </MyCar>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>合计:</td>
+          <td colspan="5"></td>
+        </tr>
+      </tfoot>
+    </table>
+  </div>
+</template>
+
+<script>
+import MyCar from './components/Mycar.vue';
+export default {
+  data() {
+    return {
+      goodList: [
+        {
+          name: '诸葛亮',
+          price: 1000,
+          num: 1,
+          checked: false,
+        },
+        {
+          name: '蔡文姬',
+          price: 1500,
+          num: 1,
+          checked: false,
+        },
+        {
+          name: '妲己',
+          price: 2000,
+          num: 1,
+          checked: false,
+        },
+        {
+          name: '鲁班',
+          price: 2200,
+          num: 1,
+          checked: false,
+        },
+      ],
+    };
+  },
+  components: {
+    MyCar,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/Mycar.vue
@@ -0,0 +1,41 @@
+<template>
+  <tr>
+    <td><input type="checkbox" name="" id="" /></td>
+    <td>{{ item.name }}</td>
+    <td>{{ item.price }}</td>
+    <td>
+      <!-- <button>你好</button> -->
+      <a
+        href="javascript:void(0)"
+        @click="item.num <= 0 ? (item.num = 0) : item.num--"
+        >-</a
+      >
+      {{ item.num }}
+      <a href="javascript:void(0)" @click="item.num++">+</a>
+    </td>
+    <td>{{ item.price * item.num }}</td>
+    <td><a href="javascript:void(0)" @click="del(item.name)">删除</a></td>
+  </tr>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  methods: {
+    // 删除功能
+    del(val) {
+      const index = this.$parent.goodList.findIndex((ele) => {
+        return ele.name == val;
+      });
+      this.$parent.goodList.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style></style>
