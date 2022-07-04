5455ce9e61c3f3c27a5f5a8e9a88d3be5e7f101b
lxh15 <1392649188@qq.com>
2022-07-04 19:20:05 +0800

    Dog组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  
+  <div>
+    <!-- 把组件当普通标签用 -->
+    <!-- <my-dog></my-dog> -->
+    <MyDog></MyDog>
+  </div>
 </template>
 
 <script>
+// 引入子组件
+// 注册子组件
+import MyDog from './components/Dog.vue';
 export default {
-
-}
+  components: {
+    MyDog,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
