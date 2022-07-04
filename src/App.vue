c426c4a632158d4e9d119bc99ea2640acab231d9
lxh15 <1392649188@qq.com>
2022-07-05 00:26:48 +0800

    封装按钮组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    
+    <MyButton title="登录按钮" type="primary" @Myclick="MyclickFn"> </MyButton>
   </div>
 </template>
 
 <script>
+import MyButton from './components/Button1.vue';
 export default {
-
-}
+  components: {
+    MyButton,
+  },
+  methods:{
+    MyclickFn(){
+      alert('111')
+    }
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/Button1.vue
@@ -0,0 +1,62 @@
+<template>
+  <button :class="`on-button-${colorStr}`" @click="Myclick">{{ title }}</button>
+  <!-- 
+    封装一组按钮插件
+    1. 按钮的内容可以支持自定义
+    2. 按钮的类型只有4种 primary success warning danger，
+    需入其他的需要校验
+   -->
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '按钮组件',
+    },
+    type: {
+      type: String,
+      default: 'blur',
+      validator(val) {
+        return ['blur', 'yellow', 'red', 'green'];
+      },
+    },
+  },
+  data() {
+    return {
+      colorStr: 'blur',
+    };
+  },
+  methods: {
+    Myclick(){
+      this.$emit('Myclick')
+    }
+  }
+};
+</script>
+<style>
+button {
+  color: #fff;
+  outline: none;
+  border: none;
+  width: 150px;
+  height: 50px;
+  font-size: 50px;
+  padding: 8px 24px;
+  border-radius: 4px;
+  font-size: 18px;
+}
+.on-button-blur {
+  background-color: #409eff;
+}
+.on-button-green {
+  background-color: #67c23a;
+}
+.on-button-yellow {
+  background-color: #e6a23c;
+}
+.on-button-red {
+  background-color: #f56c6c;
+}
+</style>
