5a4a01eb5aae8b0324e885e9252bf85876229132
Eternallyking <2499105321@qq.com>
2022-07-05 11:42:54 +0800

    完成了作业

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,65 @@
 <template>
   <div>
-    <table border="1" width="700" style="border-collapse: collapse">
-      <caption>
-        购物车
-      </caption>
-      <thead>
-        <tr>
-          <th><input type="checkbox" v-model="isall" /> <span>全选</span></th>
-          <th>名称</th>
-          <th>价格</th>
-          <th>数量</th>
-          <th>总价</th>
-          <th>操作</th>
-        </tr>
-      </thead>
-      <tbody>
-        <MyTick
-          v-for="(item, index) in goodList"
-          :key="index"
-          :obj="item"
-        ></MyTick>
-      </tbody>
-      <tfoot>
-        <tr>
-          <td>合计:</td>
-          <td colspan="5">总件数：{{ count }} 总价格：{{ allprice }}</td>
-        </tr>
-      </tfoot>
-    </table>
+    <MyButton v-for="item in list" :key="item" :title="item.button" :type="item.color" @myclick='myclickfn'></MyButton>
   </div>
 </template>
 
 <script>
-import MyTick from "./components/MyTick.vue";
+import MyButton from './components/ButtonSubassembly.vue'
 export default {
   data() {
     return {
-      goodList: [
-        {
-          name: "诸葛亮",
-          price: 1000,
-          num: 1,
-          checked: false,
-        },
-        {
-          name: "蔡文姬",
-          price: 1500,
-          num: 1,
-          checked: false,
-        },
-        {
-          name: "妲己",
-          price: 2000,
-          num: 1,
-          checked: false,
-        },
-        {
-          name: "鲁班",
-          price: 2200,
-          num: 1,
-          checked: false,
-        },
-      ],
+      list:[{
+        button:'默认按钮',
+        color:'warning'
+      },
+      {
+        button:'主要按钮',
+        color:'primary'
+      },
+      {
+        button:'成功按钮',
+        color:'success'
+      },
+      {
+        button:'警告按钮',
+        color:'danger'
+      },
+      ]
     };
   },
-  components: {
-    MyTick,
-  },
-  methods: {},
-  computed: {
-    isall: {
-      set(checked) {
-        this.goodList.forEach((ele) => (ele.checked = checked));
-      },
-      get() {
-        return this.goodList.every((ele) => ele.checked);
-      },
-    },
-    count() {
-      return this.goodList.reduce((sum, next) => (sum += next.num), 0);
-    },
-    allprice(){
-      return this.goodList.reduce((sum, next) => sum+=next.price*next.num, 0);
+  methods:{
+    myclickfn(){
+      alert('成功')
     }
   },
-
+  components: {
+    MyButton
+  },
+  computed: {
+  },
 };
 </script>
 
-<style></style>
+<style>
+button {
+  color: #fff;
+  outline: none;
+  border: none;
+  padding: 8px 24px;
+  border-radius: 4px;
+  font-size: 18px;
+}
+.primary{
+  background-color: aqua;
+}
+.success{
+  background-color: greenyellow;
+}
+.warning{
+  background-color: whitesmoke;
+}
+.danger{
+  background-color: yellow;
+}
+</style>
