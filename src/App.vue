8aa1fd8c21dda658447d5128fea345e40c2a8615
lxh15 <1392649188@qq.com>
2022-07-05 13:33:38 +0800

    封装按钮组件完成

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
-  <div>
-    <MyButton title="登录按钮" type="primary" @Myclick="MyclickFn"> </MyButton>
-  </div>
+  
 </template>
 
 <script>
-import MyButton from './components/Button1.vue';
 export default {
-  components: {
-    MyButton,
-  },
-  methods:{
-    MyclickFn(){
-      alert('111')
-    }
-  }
-};
+
+}
 </script>
 
-<style></style>
+<style>
+
+</style>
