b3b232a0d751f5e316df75a8f7577728e57a5a3f
kelly227800 <2529303733@qq.com>
2022-07-05 14:16:50 +0800

    练习了购物车案例，新建了MyTrApp.vue保存App.vue代码

---
+++ src/components/MyTr.vue
@@ -0,0 +1,31 @@
+<template>
+  <tr>
+    <td><input type="checkbox" v-model="info.checked"></td>
+    <td>{{info.name}}</td>
+    <td>{{info.price}}</td>
+    <td><button @click="info.num = --info.num >= 0? info.num : 0"> - </button> {{info.num}} <button @click="info.num++"> + </button></td>
+    <td>{{info.price * info.num}}</td>
+    <td><button @click = "del(info.name)">删除</button></td>
+  </tr>
+</template>
+
+<script>
+export default {
+    props: {
+        info: {
+            type: Object,
+            default: () => {{}}
+        }
+    },
+    methods: {
+        del(val) {
+            const index = this.$parent.goodList.findIndex(ele => ele.name ==val);
+            this.$parent.goodList.splice(index, 1);
+        },
+    }
+}
+</script>
+
+<style>
+
+</style>
