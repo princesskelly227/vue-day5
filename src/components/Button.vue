cc46427eb0bcb575c8442ecaf40f14815fd27e63
kelly227800 <2529303733@qq.com>
2022-07-05 11:35:27 +0800

    练习了按钮案例，新建了ButtonApp.vue保存App.vue代码

---
+++ src/components/Button.vue
@@ -0,0 +1,57 @@
+<template>
+<div>
+    <button :class = "type" @click="clickFn">{{content}}</button>
+</div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return{
+
+        }
+    },
+    props:{
+        type: {
+            type: String,
+            default: "primary",
+            validator(val) {
+                return ["primary", "success", "warning", "danger"].includes(val)
+            }
+        },
+        content: {
+            type: String,
+            default: "主要按钮"
+        }
+    },
+    methods: {
+        clickFn(){
+            this.$emit("myclick")
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+button {
+  color: #fff;
+  outline: none;
+  border: none;
+  padding: 8px 24px;
+  border-radius: 4px;
+  font-size: 18px;
+}
+.primary {
+  background-color: skyblue;
+}
+.success {
+  background-color: hotpink;
+}
+.warning {
+  background-color: yellow;
+}
+.danger {
+  background-color: red;
+}
+</style>
